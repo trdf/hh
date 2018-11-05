4af2422a14dc9dd2f675e4c182d3d25587e78b0c
sch <myemail>
2018-11-05 17:46:19 +0100

    kosmetyka

---
+++ przepelnienie.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int  main()
+{
+    short m=0;
+    printf("%d", sizeof(m));
+
+    for(int i=0; i<=100000; i++)
+    {
+        printf("\n%d", m++);
+
+    }
+
+    return 0;
+}
+
+/*
+short ma 2 bajty = 2*8 bitow = 16 bitow
+2^15=32768 (^15 bo jest jeszcze pozycja 'zerowa', 2^0=1)
+*/
